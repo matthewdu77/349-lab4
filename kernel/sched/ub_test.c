6727ab05cb9ea5efc2bf7a92bf71d691aaa8ed4a
vhu <victor.y.hu@gmail.com>
2014-12-05 19:17:21 -0500

    implemented ub testing

--- kernel/sched/ub_test.c
+++ kernel/sched/ub_test.c
@@ -13,6 +13,21 @@
 #include <exports.h>
 #endif
 
+static const double u[OS_AVAIL_TASKS] = { 
+  1.000000, 0.828427, 0.779763, 0.756828, 0.743492,
+  0.734772, 0.728627, 0.724062, 0.720538, 0.717735,
+  0.715452, 0.713557, 0.711959, 0.710593, 0.709412,
+  0.708381, 0.707472, 0.706666, 0.705946, 0.705298,
+  0.704713, 0.704182, 0.703698, 0.703254, 0.702846,
+  0.702469, 0.702121, 0.701798, 0.701497, 0.701217,
+  0.700955, 0.700709, 0.700478, 0.700261, 0.700056,
+  0.699863, 0.699681, 0.699508, 0.699343, 0.699188,
+  0.699040, 0.698898, 0.698764, 0.698636, 0.698513,
+  0.698396, 0.698284, 0.698176, 0.698073, 0.697974,
+  0.697879, 0.697788, 0.697700, 0.697615, 0.697533,
+  0.697455, 0.697379, 0.697306, 0.697235, 0.697166,
+  0.697100, 0.697036, 0.696974
+}
  
 /**
  * @brief Perform UB Test and reorder the task list.
@@ -29,6 +44,9 @@
  */
 int assign_schedule(task_t** tasks, size_t num_tasks)
 {
+  if (num_tasks > OS_AVAIL_TASKS)
+    return 0;
+
   size_t i, j;
   // selection sort is easy to code
   for (i = 0; i < num_tasks; i++)
@@ -45,6 +63,23 @@ int assign_schedule(task_t** tasks, size_t num_tasks)
     tasks[i] = tasks[max];
     tasks[max] = temp;
   }
-	return 1;
+
+  // performing the UB test
+  for (i = 0; i < num_tasks; i++)
+  {
+    double admiss = 0.0;
+    
+    for (j = 0; j <= i; j++)
+    {
+      admiss += (tasks[j]->C / tasks[j]->T);
+    }
+
+    curr_u += (tasks[i]->B / tasks[i]->T);
+
+    if (admiss >= u[i])
+      return 0;
+  }
+  
+  return 1;
 }
-	
+  
